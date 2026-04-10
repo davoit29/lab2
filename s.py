985338d1a8c41e7fedc873cc0ab11b0f4c308fcd
davoit29 <166952608+davoit29@users.noreply.github.com>
2026-04-10 23:25:25 +0300

    Refactor main function and input handling
    Refactor main function to improve readability and structure.

--- s.py
+++ s.py
@@ -1,33 +1,28 @@
 import time
 
 def calc(x):
-
     return x**2 - x**2 + x*4 - x*5 + x + x
 
-def main():
-    x = 10
+x = 10
+
+while 1==1:
+    n = input()
+
+    if not n.isdigit():
+        break
     
-    while 1==1:
-        n = input()
+    n = int(n)
+    
+    start_time = time.time()
 
-        if not n.isdigit():
-            break
-        
-        n = int(n)
-        
-        start_time = time.time()
- 
-        res = 0
+    res = 0
 
-        for i in range(n):
-            res = calc(x)
-            
-        end_time = time.time()
+    for i in range(n):
+        res = calc(x)
         
-        duration = end_time - start_time
-        
-        print(f"Время : {duration} s")
-        print(f"Итог : {res}")
-
-if __name__ == "__main__":
-    main()
+    end_time = time.time()
+    
+    duration = end_time - start_time
+    
+    print(f"Время : {duration} s")
+    print(f"Итог : {res}")
